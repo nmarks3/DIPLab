e2fb5d7f93b20ea17f4b146cd623e3f8cc204789
nmarks3 <nmarks3@L11705042768.wctc.msft>
2015-02-19 15:13:03 -0600

    Done!

--- src/Startup.java
+++ src/Startup.java
@@ -1,5 +1,4 @@
 
-
 // An useful import if you need it.
 import java.text.NumberFormat;
 // Another useful import if you need it.
@@ -9,33 +8,29 @@ import javax.swing.JOptionPane;
  * Just a test class for input and output.
  *
  * 1. Create instances of low-level classes using Liskov Substitution principle.
- * 2. Create an instance of your high-level class.
- * 3. Pass one of your calculator instances to the high-level class and
- *    check the results.
- * 4. Now switch to a different calculator instance and pass that to the
- *    high-level class. Did it work? Are the low-level instances
- *    interchangeable? The DIP requires this.
- * 
+ * 2. Create an instance of your high-level class. 3. Pass one of your
+ * calculator instances to the high-level class and check the results. 4. Now
+ * switch to a different calculator instance and pass that to the high-level
+ * class. Did it work? Are the low-level instances interchangeable? The DIP
+ * requires this.
+ *
  * @author your name goes here
  */
 public class Startup {
-    
- 
+
     public static void main(String[] args) {
-        
+
         double bill = 100;
-        
-        
+
         TipCalculator waiter = new FoodServiceTipCalculator(ServiceQuality.GOOD, bill);
-        TipCalculator bagger = new BaggageServiceTipCalculator(ServiceQuality.FAIR,10);
-        
+
         TipService tip = new TipService();
-        
+
         double tipAmt = tip.calculateTip(waiter);
-        
+
         System.out.println("Tip amount: $" + tipAmt);
         System.out.println("Your total amount due: $" + (tipAmt + bill));
-        
+
     }
 
 }
